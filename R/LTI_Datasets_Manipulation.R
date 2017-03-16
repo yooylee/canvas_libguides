2eba08cf0f56fa6352a0aef721f8ac721b9cf1a2
yooylee <glide18@hotmail.com>
2017-03-16 16:34:58 -0400

    Name change for datasets

--- R/LTI_Datasets_Manipulation.R
+++ R/LTI_Datasets_Manipulation.R
@@ -1,12 +1,12 @@
-#. 1. Load Data using csv 
+#. 1. Load Data using csv
 ####. You can load your data using excel format using xlsx package
 
-canvas <- read.csv("File_Name.csv", header=TRUE)
+LibGuides <- read.csv("File_Name.csv", header=TRUE)
 
 #. 2. Remove unnecessary courses
 ####. Using Course Department Code like ANAT (Anatomy) LAW (LAW)
 
-canvas <- filter(canvas, !grepl("-Department_Code-", CourseID),
+LibGuides <- filter(canvas, !grepl("-Department_Code-", CourseID),
                     !grepl("-Department_Code-", CourseID),
                     !grepl("-Department_Code-", CourseID),
                     !grepl("-Department_Code-", CourseID))
@@ -17,25 +17,25 @@ canvas <- filter(canvas, !grepl("-Department_Code-", CourseID),
 ####. Course Guide: It starts with Department Code and Number like BUS X103 except I-Core Research and Bloomberg guides.
 ######. Therefore, [[:digit:]] in the regular expression identifies any guides containing number in the guide name.
 
-canvas <- within(canvas, {
-  guide_type = ifelse(grepl("Introduction to Library Research", Guide), "Generic Research Guide", 
-                      ifelse(grepl("[[:digit:]]", Guide), "Course Guide", 
+LibGuides <- within(LibGuides, {
+  guide_type = ifelse(grepl("Introduction to Library Research", Guide), "Generic Research Guide",
+                      ifelse(grepl("[[:digit:]]", Guide), "Course Guide",
                              ifelse(grepl("Guide: I-Core Research Guide", Guide), "Course Guide",
                                     ifelse(grepl("Guide: Bloomberg Guide", Guide), "Course Guide",
                                            ifelse(grepl("Library LTI Page", Guide), "LTI Default", "Subject Guide")
                                     ))))
 })
 
-#. 4. Add School 
+#. 4. Add School
 
-canvas <- within(canvas, {
-  School = ifelse(grepl("-AFRO-", CourseID), "Liberal Arts", 
+LibGuides <- within(LibGuides, {
+  School = ifelse(grepl("-AFRO-", CourseID), "Liberal Arts",
                   ifelse(grepl("-AMST", CourseID), "Liberal Arts", "Last Course"))
 })
 
 #. 5. Add Department
 
-canvas <- within(canvas, {
-  Department = ifelse(grepl("-AFRO-", CourseID), "Afro-American Studies", 
+LibGuides <- within(LibGuides, {
+  Department = ifelse(grepl("-AFRO-", CourseID), "Afro-American Studies",
                   ifelse(grepl("-AMST", CourseID), "American Studies", "Last Course"))
 })
